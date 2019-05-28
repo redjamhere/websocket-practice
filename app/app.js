5235dbca52f2eede648c1a3ca7819304376301c3
redjamhere <aynur.zaymanov@mail.ru>
2019-05-28 10:21:36 +0300

    simple-websocket-app

---
+++ app/app.js
@@ -0,0 +1,34 @@
+const status = document.getElementById('status')
+const messages = document.getElementById('messages')
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+
+const ws = new WebSocket('ws://localhost:3000')
+
+var i = 0
+
+function setStatus(value) {
+  status.innerHTML = value
+}
+
+function printMessage(value) {
+  const li = document.createElement('li')
+
+  li.innerHTML = value
+  messages.appendChild(li)
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  ws.send(input.value)
+  input.value = ''
+})
+
+ws.onopen = () => setStatus('Вы подключены')
+
+ws.onclose = () => setStatus('Сервер не доступен')
+
+ws.onmessage = (response) => {
+  printMessage(response.data)
+}
